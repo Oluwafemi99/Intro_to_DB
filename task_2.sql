e08d05fcf2fb89d5e773bff5fa860b2dba79eec9
oluwafemi <oluwafemidojumo@gmail.com>
2025-01-30 19:12:41 +0200

    new tables

--- task_2.sql
+++ task_2.sql
@@ -1,32 +1,32 @@
-CREATE TABLE IF NOT EXISTS books(
+CREATE TABLE IF NOT EXISTS Books(
 book_id INT AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(130) NOT NULL,
 price DOUBLE,
 publication_date DATE,
-FOREIGN KEY (author_id) REFERENCES authors(author_id)
+FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE IF NOT EXISTS authors(
+CREATE TABLE IF NOT EXISTS Authors(
 author_id INT AUTO_INCREMENT PRIMARY KEY,
 author_name VARCHAR(215) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS customers(
+CREATE TABLE IF NOT EXISTS Customers(
 customer_id INT AUTO_INCREMENT PRIMARY KEY,
 customer_name VARCHAR(215) NOT NULL,
 email VARCHAR(215) UNIQUE,
 address TEXT
 );
 
-CREATE TABLE IF NOT EXISTS orders(
+CREATE TABLE IF NOT EXISTS Orders(
 order_id INT AUTO_INCREMENT PRIMARY KEY,
-FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 order_date DATE
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details(
 orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-FOREIGN KEY (order_id) REFERENCES orders(order_id),
-FOREIGN KEY (book_id) REFERENCES books(book_id),
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id),
 quantity DOUBLE
 );
